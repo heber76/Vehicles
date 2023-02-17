26dceff8d09dd021631353f3a8e76bf51adf9f42
heber ochoa prieto <heber76@hotmail.com>
2023-02-17 11:22:25 -0600

    Add CRUD DocumentType and Brand

---
+++ Vehicles.API/Controllers/BrandsController.cs
@@ -0,0 +1,157 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Vehicles.API.Data;
+using Vehicles.API.Data.Entities;
+
+namespace Vehicles.API.Controllers
+{
+    public class BrandsController:Controller
+    {
+        private readonly DataContext _context;
+
+        public BrandsController(DataContext context)
+        {
+            _context = context;
+        }
+
+        // GET: VehicleTypes
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Brands.ToListAsync());
+        }
+
+
+        // GET: VehicleTypes/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: VehicleTypes/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Brand brand )
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Add(brand);
+                    await _context.SaveChangesAsync();
+                    return RedirectToAction(nameof(Index));
+                }
+                catch (DbUpdateException dbUpdateException)
+                {
+
+                    if (dbUpdateException.InnerException.Message.Contains("duplicate"))
+                    {
+                        ModelState.AddModelError(string.Empty, "Ya existe esta Marca.");
+
+                    }
+                    else
+                    {
+                        ModelState.AddModelError(string.Empty, dbUpdateException.InnerException.Message);
+                    }
+
+                }
+                catch (Exception exception)
+                {
+
+                    ModelState.AddModelError(string.Empty, exception.Message);
+                }
+
+            }
+            return View(brand);
+        }
+
+        // GET: VehicleTypes/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Brand bran = await _context.Brands.FindAsync(id);
+            if (bran == null)
+            {
+                return NotFound();
+            }
+            return View(bran);
+        }
+
+        // POST: VehicleTypes/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, Brand bran)
+        {
+            if (id != bran.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(bran);
+                    await _context.SaveChangesAsync();
+                    return RedirectToAction(nameof(Index));
+                }
+                catch (DbUpdateException dbUpdateException)
+                {
+
+                    if (dbUpdateException.InnerException.Message.Contains("duplicate"))
+                    {
+                        ModelState.AddModelError(string.Empty, "Ya existe esta Marca.");
+
+                    }
+                    else
+                    {
+                        ModelState.AddModelError(string.Empty, dbUpdateException.InnerException.Message);
+                    }
+
+                }
+                catch (Exception exception)
+                {
+
+                    ModelState.AddModelError(string.Empty, exception.Message);
+                }
+
+            }
+            return View(bran);
+        }
+
+        // GET: VehicleTypes/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Brand bran = await _context.Brands
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (bran == null)
+            {
+                return NotFound();
+            }
+
+            _context.Brands.Remove(bran);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+
+        }
+
+
+
+    }
+}
---
+++ Vehicles.API/Data/Entities/DocumentType.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Vehicles.API.Data.Entities
+{
+    public class DocumentType
+    {
+
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "El campo{0} es obligatorio.")]
+        [Display(Name = "Tipo de Documento")]
+        [MaxLength(50, ErrorMessage = "El campo {0} no puede tener mas de {1} carácteres.")]
+        public string Description { get; set; }
+
+    }
+}
