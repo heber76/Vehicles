66a021a025be3333f150b1687050175155d11cd3
heber ochoa prieto <heber76@hotmail.com>
2023-02-17 09:53:28 -0600

    Add CRUD Procedure

--- Vehicles.API/Data/DataContext.cs
+++ Vehicles.API/Data/DataContext.cs
@@ -12,12 +12,14 @@ namespace Vehicles.API.Data
         }
 
         public DbSet<VehicleType> VehicleTipes { get; set; }
+        public DbSet<Procedure> Procedures { get; set; }
 
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
             modelBuilder.Entity<VehicleType>().HasIndex(x => x.Description).IsUnique();
+            modelBuilder.Entity<Procedure>().HasIndex(x => x.Description).IsUnique();
 
         }
     }
